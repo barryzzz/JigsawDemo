d032962d834ae7f2d442a4f367ed41b91d02245f
newtonker <newtonker@gmail.com>
2016-03-13 14:06:29 +0800

    1.增加了权限判断

--- app/src/main/java/com/newtonker/jigsawdemo/activity/SelectPhotoActivity.java
+++ app/src/main/java/com/newtonker/jigsawdemo/activity/SelectPhotoActivity.java
@@ -1,7 +1,11 @@
 package com.newtonker.jigsawdemo.activity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
@@ -9,6 +13,7 @@ import android.support.v7.widget.ListPopupWindow;
 import android.support.v7.widget.OrientationHelper;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.AdapterView;
@@ -62,6 +67,28 @@ public class SelectPhotoActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_photo);
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        {
+            if (!Settings.System.canWrite(this))
+            {
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, 2909);
+            }
+            else
+            {
+                // continue with your code
+                init();
+            }
+        }
+        else
+        {
+            // continue with your code
+            init();
+        }
+
+    }
+
+    private void init()
+    {
         // 初始化
         directories = new ArrayList<>();
         selectedPhotosPath = new ArrayList<>();
@@ -79,6 +106,27 @@ public class SelectPhotoActivity extends AppCompatActivity
         });
 
         initView();
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults)
+    {
+        switch (requestCode)
+        {
+        case 2909:
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED)
+            {
+                init();
+            }
+            else
+            {
+                Log.e("Permission", "Denied");
+            }
+            break;
+        default:
+            break;
+        }
     }
 
     /**
